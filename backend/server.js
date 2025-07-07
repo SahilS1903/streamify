750bf52e60ab48fad34f8565cc85c43828a17067
Sahil Shah <sahildshah1903@gmail.com>
2025-07-07 18:48:17 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,17 @@ import chatRoutes from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: ["https://streamify-seven-xi.vercel.app", "http://localhost:5173"],
+    origin: "http://localhost:5173", // your Vite frontend URL
     credentials: true,
   })
 );
@@ -22,6 +25,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
