ba2df52e9606d131f36afcd87a6c8e6d11fb1ee2
Sahil Shah <sahildshah1903@gmail.com>
2025-07-07 18:21:35 +0530

    commit 1

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL , // your Vite frontend URL
+    origin: ["https://streamify-seven-xi.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
