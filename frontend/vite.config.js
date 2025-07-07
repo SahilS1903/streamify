79835f4ae7d3267e3f93687546221a0a1bed8cd0
Sahil Shah <sahildshah1903@gmail.com>
2025-07-07 18:04:44 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      port: env.VITE_PORT,
+      proxy: {
+        "/api": env.VITE_BACKEND_URL,
+      },
+    },
+    
+  };
+});
