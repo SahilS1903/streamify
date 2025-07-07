95451db36836ab4a5bc1b0c9d1a5d15e0740fd12
Sahil Shah <sahildshah1903@gmail.com>
2025-07-07 17:43:58 +0530

    final commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : import.meta.env.BACKEND_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
